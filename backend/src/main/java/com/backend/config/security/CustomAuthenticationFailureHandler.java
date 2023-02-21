00bcfcff5213e6e6d0ec64a5084724d8292f59a7
cyccc95 <cyccc95@naver.com>
2023-02-21 23:45:06 +0900

    Add EntryAuthenticationFailureHandler

---
+++ backend/src/main/java/com/backend/config/security/CustomAuthenticationFailureHandler.java
@@ -0,0 +1,22 @@
+package com.backend.config.security;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@Component
+// AuthenticationFailureHandler를 implements 받아 로그인 실패 시 핸들링하는 역할
+public class CustomAuthenticationFailureHandler implements AuthenticationFailureHandler {
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        response.sendRedirect("http://localhost:8080/api/login/fail");
+    }
+}
