c7869886ad6641b4a0613f92ffe0160f222a9102
cyccc95 <cyccc95@naver.com>
2023-02-21 23:46:25 +0900

    Add SignUp Form

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,7 +1,49 @@
-import React from "react";
+import React, { useState } from 'react';
 
 const SignUp = () => {
-  return <div>회원가입</div>;
+  const [data, setData] = useState({
+    userId: '',
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    setData({
+      ...data,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(data);
+  };
+
+  return (
+    <>
+      <form>
+        <input
+          type="text"
+          placeholder="아이디"
+          name="userId"
+          onChange={handleChange}
+        ></input>
+        <input
+          type="email"
+          placeholder="이메일"
+          name="email"
+          onChange={handleChange}
+        ></input>
+        <input
+          type="password"
+          placeholder="비밀번호"
+          name="password"
+          onChange={handleChange}
+        ></input>
+        <button onClick={handleSubmit}>회원가입</button>
+      </form>
+    </>
+  );
 };
 
 export default SignUp;
