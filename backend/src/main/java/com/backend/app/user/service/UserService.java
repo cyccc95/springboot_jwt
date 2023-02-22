6d29bcdff50c8ee36fb1a8a2dab653f7a602aa62
cyccc95 <cyccc95@naver.com>
2023-02-22 23:50:34 +0900

    Delete UserService updateUser

--- backend/src/main/java/com/backend/app/user/service/UserService.java
+++ backend/src/main/java/com/backend/app/user/service/UserService.java
@@ -5,7 +5,7 @@ import com.backend.app.user.entity.User;
 import com.backend.app.user.repository.UserRepository;
 import com.backend.common.model.CustomException;
 import com.backend.common.model.StatusCode;
-import com.backend.common.model.UserType;
+import com.backend.common.model.type.UserType;
 import com.backend.common.util.CheckUtil;
 import lombok.RequiredArgsConstructor;
 //import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -29,6 +29,7 @@ public class UserService {
         } else if (CheckUtil.isEmptyString(userInfo.getPassword())) {
             throw new CustomException(StatusCode.CODE_603);
         }
+
 //        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         User user = User.builder()
                 .loginId(userInfo.getLoginId())
@@ -39,26 +40,7 @@ public class UserService {
                 .build();
         user.setCreateAt(LocalDateTime.now());
         user.setUpdateAt(LocalDateTime.now());
-        userRepository.save(user).toUserInfoDTO();
+        userRepository.save(user).toUserBasicDTO();
     }
 
-    @Transactional
-    public void updateUser(UserDTO.Update userInfo) throws CustomException {
-        if (CheckUtil.isEmptyString(userInfo.getNickname())) {
-            throw new CustomException(StatusCode.CODE_602);
-        } else if (CheckUtil.isEmptyString(userInfo.getPassword())) {
-            throw new CustomException(StatusCode.CODE_603);
-        }
-//        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-
-        User preUser = userRepository.findByLoginId(userInfo.getLoginId());
-        User user = User.builder()
-                        .loginId(userInfo.getLoginId())
-                        .nickname(userInfo.getNickname())
-                        .password(userInfo.getPassword())
-
-        user.setCreateAt(LocalDateTime.now());
-        user.setUpdateAt(LocalDateTime.now());
-        userRepository.save(user).toUserInfoDTO();
-    }
 }
