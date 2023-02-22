c36eab48d907f9f680cc7bb43fa5d1fa3d1fefb7
cyccc95 <cyccc95@naver.com>
2023-02-22 23:50:15 +0900

    Add UserDetailServiceImpl

---
+++ backend/src/main/java/com/backend/app/user/service/UserDetailsServiceImpl.java
@@ -0,0 +1,30 @@
+package com.backend.app.user.service;
+
+import com.backend.app.user.entity.User;
+import com.backend.app.user.repository.UserRepository;
+import com.backend.config.security.UserPrincipal;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String loginId) throws UsernameNotFoundException {
+        User user = userRepository.findByLoginId(loginId);
+        return user == null ? null : UserPrincipal.create(user);
+    }
+
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByIdx(Long idx) {
+        User user = userRepository.findByIdx(idx);
+        return user == null ? null : UserPrincipal.create(user);
+    }
+}
