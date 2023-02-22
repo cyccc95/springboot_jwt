d962fce0e3b755cdfc43b0d20c8c01d11527c0db
cyccc95 <cyccc95@naver.com>
2023-02-22 23:52:44 +0900

    Add UserPrincipal

---
+++ backend/src/main/java/com/backend/config/security/UserPrincipal.java
@@ -0,0 +1,67 @@
+package com.backend.config.security;
+
+import com.backend.app.user.entity.User;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Getter
+@RequiredArgsConstructor
+public class UserPrincipal implements UserDetails {
+
+    private final User user;
+
+    public static UserPrincipal create(User user) {
+        return new UserPrincipal(user);
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    public Long getIdx() {
+        return this.user.getIdx();
+    }
+
+    public String getNickName() {
+        return this.user.getNickname();
+    }
+
+    public String getLoginId() {
+        return this.user.getLoginId();
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
