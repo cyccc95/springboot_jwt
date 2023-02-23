a728d32f7f6f756d856e1e8a85edef51133bead0
cyccc95 <cyccc95@naver.com>
2023-02-23 21:21:48 +0900

    Delete backend2

--- backend2/src/main/java/com/coin/common/model/type/Authority.java
+++
@@ -1,5 +0,0 @@
-package com.coin.common.model.type;
-
-public enum Authority {
-    ROLE_USER, ROLE_ADMIN
-}
--- backend2/src/main/java/com/coin/config/jpa/JpaAuditingConfig.java
+++
@@ -1,9 +0,0 @@
-package com.coin.config.jpa;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-@EnableJpaAuditing
-@Configuration
-public class JpaAuditingConfig {
-}
