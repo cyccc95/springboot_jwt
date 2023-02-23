41b5c61b226a748ad9555bd14235bb966d20e8e8
cyccc95 <cyccc95@naver.com>
2023-02-23 17:24:45 +0900

    Update App.js Route Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import SignUp from './pages/SignUp';
-import SignIn from './pages/SignIn';
-import Chart from './pages/Chart';
+import HomePage from './pages/HomePage';
+import SignInPage from './pages/SignInPage';
+import SignUpPage from './pages/SignUpPage';
+import ChartPage from './pages/ChartPage';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" exact={true} element={<SignIn />} />
-      <Route path="/signUp" exact={true} element={<SignUp />} />
-      <Route path="/chart" exact={true} element={<Chart />} />
+      <Route path="/" exact={true} element={<HomePage />} />
+      <Route path="/signIn" exact={true} element={<SignInPage />} />
+      <Route path="/signUp" exact={true} element={<SignUpPage />} />
+      <Route path="/chart" exact={true} element={<ChartPage />} />
     </Routes>
   );
 }
