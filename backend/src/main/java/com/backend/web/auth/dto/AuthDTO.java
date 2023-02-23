223167f699b527ea5962f2b96bc4198b4e46a584
cyccc95 <cyccc95@naver.com>
2023-02-23 11:12:19 +0900

    Add AuthDTO

---
+++ backend/src/main/java/com/backend/web/auth/dto/AuthDTO.java
@@ -0,0 +1,13 @@
+package com.backend.web.auth.dto;
+
+import com.backend.common.model.type.UserType;
+import lombok.Data;
+
+public class AuthDTO {
+    @Data
+    public static class Create {
+        private String loginId;
+        private String password;
+        private UserType userType;
+    }
+}
