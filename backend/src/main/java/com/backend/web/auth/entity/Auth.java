13798423552647e7456427a99e23806226f99347
cyccc95 <cyccc95@naver.com>
2023-02-23 11:12:34 +0900

    Add Auth

---
+++ backend/src/main/java/com/backend/web/auth/entity/Auth.java
@@ -0,0 +1,14 @@
+package com.backend.web.auth.entity;
+
+import com.backend.web.user.entity.User;
+import com.backend.common.model.type.UserType;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class Auth implements Serializable {
+    private Long idx;
+    private UserType userType = UserType.USER;
+    private User user;
+}
