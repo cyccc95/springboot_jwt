8837334630c8e75792990aa685e1bd256b0e8c1e
cyccc95 <cyccc95@naver.com>
2023-02-24 16:14:56 +0900

    Add frontend

---
+++ frontend/src/pages/ChartPage.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
+const ChartPage = () => {
+  const [user, setUser] = useState({
+    loginId: '',
+    nickname: '',
+  });
+
+  useEffect(() => {
+    axios.defaults.headers.common[
+      'Authorization'
+    ] = `Bearer ${localStorage.getItem('accessToken')}`;
+    axios
+      .get('/api/member/1')
+      .then((response) => setUser({ ...user, ...response.data.data }))
+      .catch((error) => console.log(error));
+  }, []);
+
+  return (
+    <div style={{ width: '50%', padding: '20px' }}>
+      <h1>Chart Page</h1>
+      <div></div>
+    </div>
+  );
+};
+
+export default ChartPage;
