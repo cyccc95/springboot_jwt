ae079649b49e2dfdd1c71c7fe90dbdf122f575d5
cyccc95 <cyccc95@naver.com>
2023-02-24 14:16:52 +0900

    Update JwtAuthenticationEntryPoint comment

--- backend/src/main/java/com/backend/config/jwt/JwtAuthenticationEntryPoint.java
+++ backend/src/main/java/com/backend/config/jwt/JwtAuthenticationEntryPoint.java
@@ -8,12 +8,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+/**
+ * 인증 정보 없을 때 401 에러
+ */
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
-        // 유효한 자격증명을 제공하지 않고 접근하려 할때 401
+        // 유효한 자격증명(유저 정보)을 제공하지 않고 접근하려 할때 401
         response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
     }
 }
