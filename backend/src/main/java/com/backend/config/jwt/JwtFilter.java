a8256044afbc8a2ed307dac68cbd4a4d2a891817
cyccc95 <cyccc95@naver.com>
2023-02-24 14:17:03 +0900

    Update JwtFilter comment

--- backend/src/main/java/com/backend/config/jwt/JwtFilter.java
+++ backend/src/main/java/com/backend/config/jwt/JwtFilter.java
@@ -12,6 +12,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+/**
+ * Spring Request 앞단에 붙일 Custom Filter
+ * OncePerRequestFilter 인터페이스를 구현하기 때문에 요청을 받을때 한번만 실행
+ */
 @RequiredArgsConstructor
 public class JwtFilter extends OncePerRequestFilter {
 
@@ -20,8 +24,15 @@ public class JwtFilter extends OncePerRequestFilter {
 
     private final JwtProvider jwtProvider;
 
-    // 실제 필터링 로직은 doFilterInternal 에 들어감
-    // JWT 토큰의 인증 정보를 현재 쓰레드의 SecurityContext 에 저장하는 역할 수행
+    /**
+     * 실제 필터링 로직은 doFilterInternal 에 들어감
+     * JWT 토큰의 인증 정보를 현재 쓰레드의 SecurityContext 에 저장하는 역할 수행
+     * 실제 필터링 로직을 수행하는 곳
+     * Request Header에서 AccessToken을 꺼내고 여러가지 검사 후 유저 정보를 꺼내서 SecurityContext에 저장
+     * 가입/로그인/재발급을 제외한 모든 Request 요청은 이 필터를 거치기 때문에 토큰 정보가 없거나 유효하지 않으면 정상적으로 수행되지 않음
+     * 그리고 요청이 정상적으로 Controller까지 도착했다면 SecurityContext에 Member Idx가 존재한다는 것이 보장됨
+     * 대신 직접 DB를 조회한 것이 아니라 AccessToken에 있는 Member Idx를 꺼낸 거라서, 탈퇴로 인해 Member Idx가 DB에 없는 경우 등 예외 상황은 Service단에서 고려해야함
+     */
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException, ServletException {
 
