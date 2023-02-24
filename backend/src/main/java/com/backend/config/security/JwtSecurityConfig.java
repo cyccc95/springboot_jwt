239cc8c402e7af2ad23cd0e572c085600c9eece1
cyccc95 <cyccc95@naver.com>
2023-02-24 14:17:24 +0900

    Update JwtSecurityConfig comment

--- backend/src/main/java/com/backend/config/security/JwtSecurityConfig.java
+++ backend/src/main/java/com/backend/config/security/JwtSecurityConfig.java
@@ -8,7 +8,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-// 직접 만든 TokenProvider 와 JwtFilter 를 SecurityConfig 에 적용할 때 사용
+/**
+ * 직접 만든 TokenProvider 와 JwtFilter 를 SecurityConfig 에 적용할 때 사용
+ * JWT Filter를 Security Filter 앞에 추가
+ * SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> 인터페이스를 구현하는 구현체
+ */
 @RequiredArgsConstructor
 public class JwtSecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
     private final JwtProvider jwtProvider;
