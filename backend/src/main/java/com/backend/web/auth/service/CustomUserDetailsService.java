64a0ba7215e2bbbf960d4e3442a4ccabbe5ac834
cyccc95 <cyccc95@naver.com>
2023-02-24 14:18:13 +0900

    Update CustomUserDetailsService

--- backend/src/main/java/com/backend/web/auth/service/CustomUserDetailsService.java
+++ backend/src/main/java/com/backend/web/auth/service/CustomUserDetailsService.java
@@ -14,6 +14,11 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collections;
 
+/**
+ * UserDetailsService 인터페이스를 구현한 클래스
+ * loadUserByUsername 메서드를 오버라이드하는데 여기서 넘겨받은 UserDetails와 Authentication의 패스워드를 비교하고 검증하는 로직을 처리
+ * 물론 DB에서 username을 기반으로 값을 가져오기 때문에 아이디 존재 여부도 자동으로 검증
+ */
 @Service
 @RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
@@ -39,3 +44,12 @@ public class CustomUserDetailsService implements UserDetailsService {
         );
     }
 }
+
+/**
+ * 비밀번호 검증 과정
+ * 1. AuthService에서 AuthenticationManagerBuilder를 주입 받음
+ * 2. AuthenticationManagerBuilder에서 AuthenticationManager를 구현한 ProviderManager 생성
+ * 3. ProviderManager는 AbstractUserDetailsAuthenticationProvider의 자식 클래스인 DaoAuthenticationProvider를 주입받아서 호출
+ * 4. DaoAuthenticationProvider의 authenticate에서는 retrieveUser로 DB에 있는 사용자 정보를 가져오고 additionalAuthenticationChecks로 비밀번호 비교
+ * 5. retrieveUser 내부에서 UserDetailsService 인터페이스를 직접 구현한 CustomUserDetailsService 클래스의 오버라이드 메서드인 loadUserByUsername가 호출됨
+ */
