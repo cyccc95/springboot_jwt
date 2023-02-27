c9fc2bc52ea66edc35cbeba1ad7ecaad29e2927a
cyccc95 <cyccc95@naver.com>
2023-02-27 18:01:04 +0900

    Update HistoryCoinRepository 최신 48개를 제외한 모든 데이터를 가져오는 query 작성

--- backend/src/main/java/com/backend/web/historyCoin/repository/HistoryCoinRepository.java
+++ backend/src/main/java/com/backend/web/historyCoin/repository/HistoryCoinRepository.java
@@ -2,6 +2,12 @@ package com.backend.web.historyCoin.repository;
 
 import com.backend.web.historyCoin.entity.HistoryCoin;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface HistoryCoinRepository extends JpaRepository<HistoryCoin, Long> {
+
+    @Query(value = "select * from historycoin limit 19952", nativeQuery = true)
+    List<HistoryCoin> findAllHistoryCoins();
 }
