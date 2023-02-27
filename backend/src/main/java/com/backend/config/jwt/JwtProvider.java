3e5b7e5bc58d3a302651c88e64e6bfcab03d6937
cyccc95 <cyccc95@naver.com>
2023-02-27 18:00:35 +0900

    Update JwtProvider token_expire_time 수정

--- backend/src/main/java/com/backend/config/jwt/JwtProvider.java
+++ backend/src/main/java/com/backend/config/jwt/JwtProvider.java
@@ -28,10 +28,10 @@ public class JwtProvider {
 
     private static final String AUTHORITIES_KEY = "auth";
     private static final String BEARER_TYPE = "Bearer";
-//    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 30;            // 30분
-    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 10;            // 10초
-//    private static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 7;  // 7일
+    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 30;            // 30분
+//    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 10;            // 10초
     private static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 7;  // 7일
+//    private static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 7;  // 7일
 
     private final Key key;
 
