354a8a10b2f6cf9fb50afb1e409c2b32908f8a1f
cyccc95 <cyccc95@naver.com>
2023-02-28 14:12:35 +0900

    Update HistoryCoinRepository findByIdxBetween

--- backend/src/main/java/com/backend/web/historyCoin/repository/HistoryCoinRepository.java
+++ backend/src/main/java/com/backend/web/historyCoin/repository/HistoryCoinRepository.java
@@ -2,12 +2,10 @@ package com.backend.web.historyCoin.repository;
 
 import com.backend.web.historyCoin.entity.HistoryCoin;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface HistoryCoinRepository extends JpaRepository<HistoryCoin, Long> {
 
-    @Query(value = "select * from historycoin limit 19952", nativeQuery = true)
-    List<HistoryCoin> findAllHistoryCoins();
+    List<HistoryCoin> findByIdxBetween(long startIdx, long endIdx);
 }
