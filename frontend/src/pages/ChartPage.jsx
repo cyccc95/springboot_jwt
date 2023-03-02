9b8ebe839c5d1c4a5c40d8cea2f1611ce5bfeb39
cyccc95 <cyccc95@naver.com>
2023-03-02 11:53:22 +0900

    Add WebSocket Test

--- frontend/src/pages/ChartPage.jsx
+++ frontend/src/pages/ChartPage.jsx
@@ -1,29 +1,43 @@
 import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
-import client from '../config/axiosConfig';
+import Button from 'react-bootstrap/Button';
 
 const ChartPage = () => {
-  const [user, setUser] = useState({
-    loginId: '',
-    nickname: '',
-  });
+  let socket;
 
-  useEffect(() => {
-    const timer = setInterval(() => {
-      client
-        .get('/api/member/me')
-        .then((response) => console.log(response))
-        .catch((error) => console.log(error));
-    }, 5000);
+  const start = () => {
+    socket = new WebSocket('ws://localhost:8080/coin');
+    socket.onopen = () => {
+      console.log('연결됨');
+    };
+    socket.onmessage = (msg) => {
+      const data = JSON.parse(msg.data);
+      console.log(data);
+    };
+  };
 
-    return () => clearInterval(timer);
-  }, []);
+  const stop = () => {
+    socket.close();
+  };
 
   return (
     <div style={{ width: '50%', padding: '20px' }}>
       <h1>Chart Page</h1>
-      <div></div>
+      <Button
+        variant="primary"
+        onClick={() => {
+          start();
+        }}
+      >
+        Open
+      </Button>
+      <Button
+        variant="primary"
+        onClick={() => {
+          stop();
+        }}
+      >
+        Close
+      </Button>
     </div>
   );
 };
