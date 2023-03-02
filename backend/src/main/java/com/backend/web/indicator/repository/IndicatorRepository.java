48bff4c3ebe00b0ae12bc63ca3f9723131724491
cyccc95 <cyccc95@naver.com>
2023-03-02 11:50:50 +0900

    Add IndicatorRepository

--- backend/src/main/java/com/backend/web/indicator/repository/IndicatorRepository.java
+++ backend/src/main/java/com/backend/web/indicator/repository/IndicatorRepository.java
@@ -3,5 +3,9 @@ package com.backend.web.indicator.repository;
 import com.backend.web.indicator.entity.Indicator;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface IndicatorRepository extends JpaRepository<Indicator, Long> {
+
+    List<Indicator> findByIdxBetween(long startIdx, long endIdx);
 }
